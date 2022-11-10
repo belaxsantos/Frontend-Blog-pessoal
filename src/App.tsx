1c7e1cda513182ad89a14ca61d2017a95b7ff3da
belaxsantos <isabela2088@hotmail.com>
2022-11-10 15:31:10 -0300

    react blog-pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
 import Login from './paginas/login/Login';
-
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <div style={{ minHeight: '100vh' }}>
       <Routes>
 
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login  />} />
 
         <Route path="/home" element={<Home />} />
 
         <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
       </Routes>
+      </div>
       <Footer />
     </Router>
 
