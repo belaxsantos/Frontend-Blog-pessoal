bb6ed08bbfb8d5f293deaaa38c3a69bfedcac339
belaxsantos <isabela2088@hotmail.com>
2022-11-17 11:51:55 -0300

    task blog pessoal

--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -1,40 +1,77 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Box} from '@mui/material'; 
 import { Link } from 'react-router-dom'
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import './ListaPostagem.css';
+import Tema from '../../../models/Tema';
+import './ListaTema.css';
+import {useNavigate} from 'react-router-dom';
+import { busca } from '../../../services/Services';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
 
-function ListaPostagem() {
+function ListaTema() {
+  const [temas, setTemas] = useState<Tema[]>([])
+  let navigate = useNavigate();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+
+  useEffect(()=>{
+    if(token == ''){
+      toast.error('Você precisa estar logado', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+        });
+        navigate("/login")
+    }
+  }, [token])
+
+
+  async function getTema(){
+    await busca("/tema", setTemas, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+
+  useEffect(()=>{
+    getTema()
+  }, [temas.length])
 
   return (
     <>
+    {
+      temas.map(tema =>(
       <Box m={2} >
         <Card variant="outlined">
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              Postagens
+              Tema
             </Typography>
             <Typography variant="h5" component="h2">
-              Título
-            </Typography>
-            <Typography variant="body2" component="p">
-              Texto da Postagem
-            </Typography>
-            <Typography variant="body2" component="p">
-              Tema
+             {tema.descricao}
             </Typography>
           </CardContent>
           <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5}>
+            <Box display="flex" justifyContent="center" mb={1.5} >
 
-              <Link to="" className="text-decorator-none" >
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
                   </Button>
                 </Box>
               </Link>
-              <Link to="" className="text-decorator-none">
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" size='small' color="secondary">
                     deletar
@@ -45,7 +82,11 @@ function ListaPostagem() {
           </CardActions>
         </Card>
       </Box>
-    </>)
+      ))
+      }
+    </>
+  );
 }
 
-export default ListaPostagem;
+
+export default ListaTema;
